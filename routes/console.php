681b3cd0e77ba5ebc9b4c54ab368c485479e9aad
Kareem <Kudo.200200@gmail.com>
2024-09-25 21:35:18 +0300

    Syncing Updated Users

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Spatie\ShortSchedule\Facades\ShortSchedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+ShortSchedule::command('users:sync')->everySeconds(90); // Or every 72 seconds.
